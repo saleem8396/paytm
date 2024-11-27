0a81cb12920a07f1b9309ccb3d97825917d05f3c
saleem <saleemraja8396@gmail.com>
2024-11-27 13:27:19 +0530

    complete send card component

--- frontend/src/components/UserName.jsx
+++ frontend/src/components/UserName.jsx
@@ -1,9 +1,9 @@
 export function Username(){
      return <div className="flex ">
-        <div>
+        <div className="bg-green-500 rounded-full w-10  h-10 text-center flex flex-col justify-center">
            S
         </div>
-        <div >
+        <div className=" flex flex-col justify-center ml-3" >
             saleem
         </div>
      </div>
